9de13161d8b8a894ba20bdf0748cc77397bc285f
Mahesh Phale <mahesh.phale@techstalwarts.com>
2026-02-25 17:44:29 +0530

    Added multiple resource method

---
+++ src/main/java/com/dipcoin/api/resource/PartnerEncDecResource.java
@@ -0,0 +1,12 @@
+package com.dipcoin.api.resource;
+
+import com.dipcoin.api.model.PartnerResponse;
+
+public class PartnerEncDecResource {
+
+	public PartnerResponse encryptPartnerResponse(String referenceId, String writeValueAsString) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
